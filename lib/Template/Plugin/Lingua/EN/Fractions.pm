0cede28b0480c2433b91fe7947588f0471c9adab
Barbie <barbie@missbarbell.co.uk>
2015-01-01 16:06:02 +0000

    Import of BARBIE/Template-Plugin-Lingua-EN-Fractions-0.02.tar.gz from CPAN.
    gitpan-cpan-distribution: Template-Plugin-Lingua-EN-Fractions
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         BARBIE/Template-Plugin-Lingua-EN-Fractions-0.02.tar.gz
    gitpan-cpan-author:       BARBIE
    gitpan-cpan-maturity:     released

--- lib/Template/Plugin/Lingua/EN/Fractions.pm
+++ lib/Template/Plugin/Lingua/EN/Fractions.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = 0.01;
+$VERSION = 0.02;
 
 use base qw(Template::Plugin);
 use Template::Plugin;
@@ -80,7 +80,7 @@ recently taken over from Andrew Ford.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright (C) 2014      Barbie for Miss Barbell Productions.
+Copyright (C) 2014-2015 Barbie for Miss Barbell Productions.
 
 This distribution is free software; you can redistribute it and/or
 modify it under the Artistic Licence v2.
